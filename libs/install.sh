d38284a98622f558df346df04541958b09b0b972
Kirill Trofimov <sinnus@gmail.com>
2011-09-29 13:57:08 +0400

    Added websocket.swc dependency

--- libs/install.sh
+++ libs/install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 mvn install:install-file -Dfile=as3corelib.swc -DgroupId=com.adobe -DartifactId=as3corelib -Dversion=1.0 -Dpackaging=swc
-mvn install:install-file -Dfile=WebSocketMain.swf -DgroupId=net.gimite -DartifactId=websocket -Dversion=1.0 -Dpackaging=swc
+mvn install:install-file -Dfile=websocket.swc -DgroupId=net.gimite -DartifactId=websocket -Dversion=1.0 -Dpackaging=swc
